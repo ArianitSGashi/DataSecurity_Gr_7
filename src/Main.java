653158082bb68dd72caa43235883cc47c143affc
ArditGjyrevci <ardit.gjyrevci@student.uni-pr.edu>
2023-04-15 13:58:12 +0200

    message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Arianit");
+        System.out.println("Hello Arianit");
+
+        System.out.println("Hello Arianit");
+        System.out.println("Hello Arianit");
+        System.out.println("Hello Arianit");
+
+
     }
 }
