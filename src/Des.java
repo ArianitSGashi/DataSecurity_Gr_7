043bf9b4ba4503e793dd00fb7b5449795b1f72d4
ArianitSGashi <arianit.gashi9@student.uni-pr.edu>
2023-04-15 21:55:15 +0200

    Scanner object

--- src/Des.java
+++ src/Des.java
@@ -1,4 +1,9 @@
-import javax.crypto.Cipher;
+import javax.crypto.*;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
 public class Des {
     //creating an instance of the Cipher class for encryption
@@ -8,4 +13,11 @@ public class Des {
     //initializing vector
     private static final byte[] initialization_vector = { 22, 33, 11, 44, 55, 99, 66, 77 };
     //main() method
+    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException,
+            IllegalBlockSizeException, InvalidKeyException, InvalidAlgorithmParameterException, IOException {
+        //path of the file that we want to encrypt
+        Scanner input = new Scanner(System.in);
+        SecretKey scrtkey = KeyGenerator.getInstance("DES").generateKey();//Gjeneron nje secret(symetric)key me ane te implenetimit te libraris javax.crypto
+        //getInstance("DES") specifikon se qelsi i gjeneruar eshte des key
+    }
 }
