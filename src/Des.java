8e032a1351fa7b666104f4042de34742da0620f3
ArianitSGashi <arianit.gashi9@student.uni-pr.edu>
2023-04-15 21:50:40 +0200

    des class

---
+++ src/Des.java
@@ -0,0 +1,11 @@
+import javax.crypto.Cipher;
+
+public class Des {
+    //creating an instance of the Cipher class for encryption
+    private static Cipher encrypt;
+    //creating an instance of the Cipher class for decryption
+    private static Cipher decrypt;
+    //initializing vector
+    private static final byte[] initialization_vector = { 22, 33, 11, 44, 55, 99, 66, 77 };
+    //main() method
+}
